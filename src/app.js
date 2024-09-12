abbff53b2d0572bee19d74c4f8d4187d1845b65f
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-09-12 17:50:48 +0000

    [Generador de excusas]
    margarita1973- Se creo la funcion generadorExcusas en app.js
    Se modifico index.html creando dos lineas de texto, una de introduccion y otra con la excusa generada.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorExcusas() {
+    let quien = [
+      "El perro",
+      "Mi abuela",
+      "El cartero",
+      "Mi conejo",
+      "El vecino",
+    ];
+    let accion = ["se robó", "rompió", "chocó", "quebró", "se llevó"];
+    let que = [
+      "mi tarea",
+      "el teléfono",
+      "el auto",
+      "mi abrigo",
+      "mi computadora",
+    ];
+    let cuando = [
+      "antes de la clase",
+      "cuando estaba durmiendo",
+      "mientras hacia ejercicio",
+      "durante el almuerzo",
+      "cuando me estaba bañando",
+    ];
+    let positionQuien = Math.floor(Math.random() * quien.length);
+    let positionAccion = Math.floor(Math.random() * accion.length);
+    let positionQue = Math.floor(Math.random() * que.length);
+    let positionCuando = Math.floor(Math.random() * cuando.length);
+
+    return (
+      quien[positionQuien] +
+      " " +
+      accion[positionAccion] +
+      " " +
+      que[positionQue] +
+      " " +
+      cuando[positionCuando]
+    );
+  }
+  let paragraph = document.getElementById("excuse");
+  paragraph.innerText = generadorExcusas();
 };
